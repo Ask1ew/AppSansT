15ec167d927ea20034fb441e9dee6bf5e2b6f044
Ask1ew <99717881+Ask1ew@users.noreply.github.com>
2025-03-30 16:02:03 +0200

    Global commit

---
+++ app/src/main/java/home/homeActivity.java
@@ -0,0 +1,14 @@
+package home;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.appsanst.R;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
