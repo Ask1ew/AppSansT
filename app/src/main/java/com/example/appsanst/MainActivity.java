3a31440a5868876199d6f46c3db597a02517004c
Ask1ew <99717881+Ask1ew@users.noreply.github.com>
2025-03-31 08:27:00 +0200

    Global commit

--- app/src/main/java/com/example/appsanst/MainActivity.java
+++ app/src/main/java/com/example/appsanst/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appsanst;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,9 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(MainActivity.this, connexion.ui.login.LoginActivity.class);
-        startActivity(intent);
+        SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        if (isLoggedIn) {
+            // Rediriger vers une autre activité si l'utilisateur est connecté
+            Intent intent = new Intent(MainActivity.this, home.homeActivity.class);
+            startActivity(intent);
+        } else {
+            // Rediriger vers LoginActivity si l'utilisateur n'est pas connecté
+            Intent intent = new Intent(MainActivity.this, connexion.ui.login.LoginActivity.class);
+            startActivity(intent);
+        }
 
         finish();
     }
+
 }
