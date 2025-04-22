dd472437aacfb5fffb4178d9d2b167e76be683fa
Ask1ew <99717881+Ask1ew@users.noreply.github.com>
2025-04-22 09:16:27 +0200

    Global commit

--- app/src/main/java/home/homeActivity.java
+++ app/src/main/java/home/homeActivity.java
@@ -16,7 +16,7 @@ public class homeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        setContentView(R.layout.activity_home);
 
         // Récupérer le nom d'utilisateur depuis l'Intent
         String displayName = getIntent().getStringExtra("displayName");
