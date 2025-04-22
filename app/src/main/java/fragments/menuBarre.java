c0d4fb6efbbc44eac668665f9369c8b6c5a5878d
Ask1ew <99717881+Ask1ew@users.noreply.github.com>
2025-04-22 09:04:09 +0200

    Global commit

--- app/src/main/java/fragments/menuBarre.java
+++ app/src/main/java/fragments/menuBarre.java
@@ -1,6 +1,10 @@
 package fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.PopupMenu;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +16,12 @@ import com.example.appsanst.R;
  * Use the {@link menuBarre#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class menuBarre extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -35,7 +37,7 @@ public class menuBarre extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment menuBarre.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static menuBarre newInstance(String param1, String param2) {
         menuBarre fragment = new menuBarre();
         Bundle args = new Bundle();
@@ -57,7 +59,44 @@ public class menuBarre extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu_barre, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Clic sur le texte central : retour à LoginActivity
+        view.findViewById(R.id.textViewMenuBarre).setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), home.homeActivity.class));
+        });
+
+        // Clic sur le bouton burger : menu déroulant
+        view.findViewById(R.id.buttonBurger).setOnClickListener(v -> {
+            PopupMenu popupMenu = new PopupMenu(requireContext(), v);
+            popupMenu.getMenuInflater().inflate(R.menu.menu_accueil, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(item -> {
+                int itemId = item.getItemId();
+                if (itemId == R.id.menu_accueil) {
+                    startActivity(new Intent(getActivity(), home.homeActivity.class));
+                    return true;
+                } else if (itemId == R.id.menu_repas) {
+                    startActivity(new Intent(getActivity(), com.example.appsanst.RepasActivity.class));
+                    return true;
+                } else if (itemId == R.id.menu_entrainement) {
+                    startActivity(new Intent(getActivity(), com.example.appsanst.EntrainementActivity.class));
+                    return true;
+                } else if (itemId == R.id.menu_poids) {
+                    startActivity(new Intent(getActivity(), com.example.appsanst.PoidsActivity.class));
+                    return true;
+                } else if (itemId == R.id.menu_objectifs) {
+                startActivity(new Intent(getActivity(), com.example.appsanst.ObjectifsActivity.class));
+                return true;
+                }
+                return false;
+            });
+            popupMenu.show();
+        });
+    }
+
 }
