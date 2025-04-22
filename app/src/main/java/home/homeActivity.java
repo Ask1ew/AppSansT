bd17a9b3d6c5874eab4847e2526397f8fc395ec0
Ask1ew <99717881+Ask1ew@users.noreply.github.com>
2025-04-22 09:18:20 +0200

    Global commit

--- app/src/main/java/home/homeActivity.java
+++ app/src/main/java/home/homeActivity.java
@@ -18,12 +18,9 @@ public class homeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Récupérer le nom d'utilisateur depuis l'Intent
-        String displayName = getIntent().getStringExtra("displayName");
-
         // Afficher le message de bienvenue
         TextView welcomeText = findViewById(R.id.welcome_text);
-        welcomeText.setText(getString(R.string.welcome) + " " + displayName);
+        welcomeText.setText(getString(R.string.welcome));
 
         // Configurer le bouton pour la page Repas
         Button buttonRepas = findViewById(R.id.button_repas);
